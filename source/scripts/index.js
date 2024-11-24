bc4d59b586763bf2427fcc3b8f7c816347317e2d
VinMariani <lady.mescheryakowa@yandex.ru>
2024-11-24 15:18:14 +0300

    add  iframe with map

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,7 +2,29 @@ const nav = document.querySelector('.navigation');
 const navToggle = document.querySelector('.navigation__toggle');
 
 // eslint-disable-next-line prefer-arrow-callback
+// navToggle.addEventListener('click', function () {
+//   nav.classList.toggle('navigation--active');
+//   navToggle.classList.toggle('active');
+// });
+
+function getWindowWidth() {
+  return window.innerWidth;
+};
+
+// при ширине экрана больше 768 автоматически удаляю класс active;
+function removeActiveClass() {
+  if (getWindowWidth() > 768) {
+    nav.classList.remove('navigation--active');
+    navToggle.classList.remove('active');
+  }
+};
+
+removeActiveClass();
+
+// eslint-disable-next-line prefer-arrow-callback
 navToggle.addEventListener('click', function () {
   nav.classList.toggle('navigation--active');
   navToggle.classList.toggle('active');
 });
+
+
