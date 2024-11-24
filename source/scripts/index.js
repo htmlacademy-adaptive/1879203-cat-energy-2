57de2b86a2a571337dfb402ebb703ed1a5afb807
VinMariani <lady.mescheryakowa@yandex.ru>
2024-11-24 12:36:59 +0300

    add burger-menu

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__toggle');
+
+// eslint-disable-next-line prefer-arrow-callback
+navToggle.addEventListener('click', function () {
+  nav.classList.toggle('navigation--active');
+  navToggle.classList.toggle('active');
+});
